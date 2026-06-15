a299e3a5e61f468e65d5e9778e87bfe4a18a7a69
Jayasuriya <jayasuriyajai2005@gmail.com>
2026-06-15 12:20:16 +0530

    Added Attendance entity.

--- src/main/java/com/tatastrive/erp/JAM/Enterprises/Entity/Attendance.java
+++ src/main/java/com/tatastrive/erp/JAM/Enterprises/Entity/Attendance.java
@@ -1,4 +1,26 @@
 package com.tatastrive.erp.JAM.Enterprises.Entity;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.time.LocalDate;
+@Data
+@Entity
+@Table(name="attendance")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Attendance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long attendanceId;
+    private LocalDate date;
+    private String status;
+    private String checkInTime;
+    private String checkOutTime;
+    @ManyToOne
+    @JoinColumn(name = "employee_id")
+    private Employee employee;
+
+
+
+
 }
