81cae3acf2829d23be48fbfeeb111fe54523d433
adithyan <adithyanswaminathan04@gmail.com>
2026-06-23 10:31:58 +0530

    Mapperupdated

--- src/main/java/com/tatastrive/erp/JAM/Enterprises/mapper/AssetMapper.java
+++ src/main/java/com/tatastrive/erp/JAM/Enterprises/mapper/AssetMapper.java
@@ -2,11 +2,14 @@ package com.tatastrive.erp.JAM.Enterprises.mapper;
 import org.mapstruct.Mapper;
 import com.tatastrive.erp.JAM.Enterprises.Entity.Asset;
 import com.tatastrive.erp.JAM.Enterprises.dto.assetdto.AssetDto;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AssetMapper {
-
+    @Mapping(source = "id",target = "assetId")
+    @Mapping(source = "employee.employeeId", target = "employeeId")
+    @Mapping(source = "employee.employeeName", target = "employeeName")
     AssetDto toDTO(Asset asset);
 
-    Asset toEntity(AssetDto dto);
+
 }
